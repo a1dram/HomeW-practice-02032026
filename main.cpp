f86434d20f39af9467c87f6024922035a2249dc6
a1dram <aldram54@gmail.com>
2026-03-02 02:26:25 +0300

    Final commit: explicit interface added and task is done ya nadeus

--- main.cpp
+++ main.cpp
@@ -3,15 +3,17 @@
 template<class T>
 T* merge(const T* a, size_t sa, const T* b, size_t sb, T* c)
 {
+  // явный интерфейс: если sa или sb неверные, то UB обеспечен
+  // явный интерфейс: не факт, что a и b отсортированы
   const T* a_it = a;
-  const T* a_end = a + sa;
+  const T* a_end = a + sa; // явный интерфейс: const T* + size_t быстрый, но опасный
   const T* b_it = b;
-  const T* b_end = b + sb;
+  const T* b_end = b + sb; // то же, что и с a_end
   T* out = c;
 
   while (a_it != a_end && b_it != b_end)
   {
-    if (*b_it < *a_it) // нужен T::operator<(const T&, const T&)
+    if (*b_it < *a_it) // нужен T::operator<(const T&, const T&) 
     {
       *out = *b_it; // нужен T::operator=(const T&)
       ++b_it;
