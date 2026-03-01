8773a1be5217434dd7e068ce6084e5560fd19331
a1dram <aldram54@gmail.com>
2026-03-02 01:39:55 +0300

    Initial commit: setup iterators

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+template<class T>
+T* merge(const T* a, size_t sa, const T* b, size_t sb, T* c)
+{
+  const T* a_it = a;
+  const T* a_end = a + sa;
+  const T* b_it = b;
+  const T* b_end = b + sb;
+  T* out = c;
+
+  return out;
+}
